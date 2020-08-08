97cfcf1eb131dcbc178e0d6529efcc1cf572556c
yelaD <dale@internold.com>
2020-08-08 10:45:58 +0800

    initial commit

---
+++ dist/js/main.js
@@ -0,0 +1,8 @@
+$(document).ready(function () {
+  $(".slider").slick({
+    prevArrow: $(".prev-btn"),
+    nextArrow: $(".next-btn"),
+    slidesToShow: 1,
+    fade: true,
+  });
+});
---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  purge: [],
+  theme: {
+    extend: {
+      colors: {
+        darkBlue: "#202046",
+        grayBlue: "#BABACF",
+        links: "hsl(228, 45%, 44%)",
+      },
+      fontFamily: {
+        inter: ["Inter", "sans-serif"],
+      },
+    },
+  },
+  variants: {},
+  plugins: [],
+};
